943b82de8c4835b49c462ddc53599958b9d7e265
Simon Gray <simongray@gmail.com>
2020-04-06 18:07:30 +0200

    basic tab styling

--- src/kuhumcst/tei_facsimile/tab.cljs
+++ src/kuhumcst/tei_facsimile/tab.cljs
@@ -1,44 +1,50 @@
 (ns kuhumcst.tei-facsimile.tab
   "Tab components that rely entirely on external, derefable state, e.g. a ratom.
-  The `state` should deref as a map of the keys:
+  The `state` should always deref as a map of the keys:
 
     :i - the index of the currently selected tab.
     :kvs - key-value pairs of tab headers and bodies.
 
-  The tab `headers` and `body` are, in principle, visually decoupled. However,
+  The tab `head` and `body` are, in principle, visually decoupled. However, a
   merged view is available in the form of the `window` component.")
 
-(defn- nth-header
+(defn- nth-label
   "The header in the tab `state` at index `n`."
   [n state]
   (let [{:keys [i kvs]} @state
-        [k _] (nth kvs n)]
-    [:label
-     [:input {:type      "radio"
+        [k v] (nth kvs n)
+        id (random-uuid)]
+    [:<> {:key id}                                          ; so checked works
+     [:input {:id        id
+              :type      "radio"
               :checked   (= n i)
               :read-only true
-              :on-click  #(swap! state assoc :i n)}]
-     k]))
+              :value     n}]
+     [:label {:for id} k]]))
 
-(defn headers
+(defn head
   "The headers available in the tab `state`."
   [state]
   (let [{:keys [kvs]} @state]
-    [:<>
+    [:form.tab-head {:on-change (fn [e]
+                                  (let [i (js/parseInt (.. e -target -value))]
+                                    (swap! state assoc :i i)))}
      (for [n (range (count kvs))]
-       ^{:key n} [nth-header n state])]))
+       [:<> {:key n}
+        [nth-label n state]
+        [:span.tab-head__spacer]])]))
 
 (defn body
   "The currently selected body in the tab `state`."
   [state]
   (let [{:keys [i kvs]} @state
         [_ v] (nth kvs i)]
-    v))
+    [:article.tab-body v]))
 
 (defn window
   "A merged view of the tab headers and the body of the currently selected tab.
   Takes tab `state` in the form described in the description of this namespace."
   [state]
-  [:article
-   [:section [headers state]]
-   [:section [body state]]])
+  [:<>
+   [head state]
+   [body state]])
