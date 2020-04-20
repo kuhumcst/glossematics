38c80d5b752f02e90ff4260c4e68b0b589b1c807
Simon Gray <simongray@gmail.com>
2020-04-20 10:08:09 +0200

    file selector for testing TEI

--- dev/user.cljs
+++ dev/user.cljs
@@ -1,53 +1,71 @@
 (ns user
   (:require [clojure.pprint :refer [pprint]]
-            [shadow.resource :as resource]
+            [shadow.resource :as sr]
             [reagent.core :as r]
             [reagent.dom :as rdom]
             [kuhumcst.recap.tabs :as tabs]
             [kuhumcst.tei-facsimile.core :as facsimile]))
 
-(def tei-examples
-  {"1151anno-anno-tei.xml"  (resource/inline "examples/tei/1151anno-anno-tei.xml")
-   "1152anno-anno-tei.xml"  (resource/inline "examples/tei/1152anno-anno-tei.xml")
-   "1153anno-anno-tei.xml"  (resource/inline "examples/tei/1153anno-anno-tei.xml")
-   "1154anno-anno-tei.xml"  (resource/inline "examples/tei/1154anno-anno-tei.xml")
-   "1155anno-anno-tei.xml"  (resource/inline "examples/tei/1155anno-anno-tei.xml")
-   "1156anno-anno-tei.xml"  (resource/inline "examples/tei/1156anno-anno-tei.xml")
-   "1157anno-anno-tei.xml"  (resource/inline "examples/tei/1157anno-anno-tei.xml")
-   "1158anno-anno-tei.xml"  (resource/inline "examples/tei/1158anno-anno-tei.xml")
-   "1159anno-anno-tei.xml"  (resource/inline "examples/tei/1159anno-anno-tei.xml")
-   "1160anno-anno-tei.xml"  (resource/inline "examples/tei/1160anno-anno-tei.xml")
-   "tei_example.xml"        (resource/inline "examples/tei/tei_example.xml")
-   "test-1307-anno-tei.xml" (resource/inline "examples/tei/test-1307-anno-tei.xml")})
+(def initial-examples
+  {"1151anno-anno-tei.xml"  (sr/inline "examples/tei/1151anno-anno-tei.xml")
+   "1152anno-anno-tei.xml"  (sr/inline "examples/tei/1152anno-anno-tei.xml")
+   "1153anno-anno-tei.xml"  (sr/inline "examples/tei/1153anno-anno-tei.xml")
+   "1154anno-anno-tei.xml"  (sr/inline "examples/tei/1154anno-anno-tei.xml")
+   "1155anno-anno-tei.xml"  (sr/inline "examples/tei/1155anno-anno-tei.xml")
+   "1156anno-anno-tei.xml"  (sr/inline "examples/tei/1156anno-anno-tei.xml")
+   "1157anno-anno-tei.xml"  (sr/inline "examples/tei/1157anno-anno-tei.xml")
+   "1158anno-anno-tei.xml"  (sr/inline "examples/tei/1158anno-anno-tei.xml")
+   "1159anno-anno-tei.xml"  (sr/inline "examples/tei/1159anno-anno-tei.xml")
+   "1160anno-anno-tei.xml"  (sr/inline "examples/tei/1160anno-anno-tei.xml")
+   "tei_example.xml"        (sr/inline "examples/tei/tei_example.xml")
+   "test-1307-anno-tei.xml" (sr/inline "examples/tei/test-1307-anno-tei.xml")})
 
-(defn mk-kvs
+(def examples
+  (r/atom initial-examples))
+
+(defn mk-tabs
   [filename]
-  (let [tei (get tei-examples filename)]
+  (let [tei (get @examples filename)]
     [["Indhold" ^{:key tei} [facsimile/tei-xml tei]]
      ["XML" [:pre [:code tei]]]
      ["Noget andet" [:<>
-                     [:h1 "Noget andet indhold"]
+                     [:h1 "Noget andet indhold for " filename]
                      [:p "Her er ikke noget lige nu."]]]]))
 
-(defonce tabs-state
-  (r/atom {:tabs (mk-kvs "tei_example.xml")}))
+(defonce state
+  (r/atom {:current-file "tei_example.xml"
+           :tabs-state   {:tabs (mk-tabs "tei_example.xml")}}))
 
 (defn set-content!
   [filename]
-  (swap! tabs-state assoc :tabs (mk-kvs filename)))
+  (swap! state assoc :current-file filename)
+  (swap! state assoc-in [:tabs-state :tabs] (mk-tabs filename)))
 
 (defn app
   []
   [:<>
    [:p {:style {:display         "flex"
                 :justify-content "flex-end"}}
-    [:select {:style     {:font-size "1rem"
-                          :padding   "0.4rem"}
-              :on-change (fn [e] (set-content! (.. e -target -value)))}
-     (for [[k _] (sort tei-examples)]
-       ^{:key k} [:option k])]]
+    (let [current-file (r/cursor state [:current-file])]
+      [:label "TEI-fil: "
+       [:select {:key           @current-file
+                 :default-value @current-file
+                 :on-change     (fn [e] (set-content! (.. e -target -value)))}
+        (for [[k _] (sort @examples)]
+          ^{:key k} [:option {:value k}
+                     k])]
+       " "
+       [:input {:aria-label "Lokal TEI-fil"
+                :type       "file"
+                :on-change  (fn [e]
+                              (when-let [file (.item e.target.files 0)]
+                                (.then (.text file)
+                                       (fn [s]
+                                         (swap! examples assoc (.-name file) s)
+                                         (set-content! (.-name file))))))}]])]
+
    ;; TODO: fix - it's error prone to copy-paste the CSS file from recap!
-   [tabs/tabs tabs-state {:tab-list-id "tei-tabs"}]])
+   [tabs/tabs (r/cursor state [:tabs-state]) {:tab-list-id "tei-tabs"}]])
 
 (def root
   (js/document.getElementById "app"))
