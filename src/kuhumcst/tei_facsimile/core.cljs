6a09f467523b76cffb305f36ca62b3a6e50661cc
Simon Gray <simongray@gmail.com>
2020-07-06 16:08:56 +0200

    using latest cuphic, rescope, recap + removed meander

--- src/kuhumcst/tei_facsimile/core.cljs
+++ src/kuhumcst/tei_facsimile/core.cljs
@@ -1,10 +1,10 @@
 (ns kuhumcst.tei-facsimile.core
   (:require [shadow.resource :as resource]
-            [meander.epsilon :as m]
-            [kuhumcst.rescope.core :as rescope]
-            [kuhumcst.rescope.hiccup :as hic]
-            [kuhumcst.rescope.style :as style]
-            [kuhumcst.rescope.formats.xml :as xml]))
+            [cuphic.core :as cup]
+            [rescope.core :as rescope]
+            [rescope.helpers :as helpers]
+            [rescope.style :as style]
+            [rescope.formats.xml :as xml]))
 
 (def prefix
   "tei")
@@ -12,7 +12,7 @@
 (def css
   (style/prefix-css prefix (resource/inline "css/tei.css")))
 
-(defn ref-title
+(defn da-type
   [type]
   (let [type->s {"conference" "denne konference"
                  "org"        "denne organisation"
@@ -23,29 +23,45 @@
                  "sender"     "denne afsender"}]
     (str "Vis mere om " (type->s type "dette"))))
 
-;; Needed for recursively postprocessing content from inside the injector.
-(declare postprocess)
+(def list-as-ul
+  (cup/transformer
+    :from '[:list +items]
+    :to (fn [{:syms [+items]}]
+          (into [:ul] (for [[tag attr & content] +items]
+                        (into [:li] content))))))
 
-(defn- injector
-  [node]
-  (some-> (m/rewrite node
-            ;; Substitute TEI lists with HTML lists.
-            [:list (m/or {:as ?attr}
-                         (m/let [?attr {}]))
-             . [:item & _ :as !x] ...]
-            [:ul ?attr
-             . [:li (m/app postprocess [:<> !x])] ...]
-
-            ;; Surround all ref attributes with hyperlinks.
-            [_ {:ref  (m/some ?ref)
-                :type ?type} & _]
+(def ref-as-anchor
+  (cup/transformer
+    :from '[? {:ref  ?ref
+               :type ?type} *]
+    :to (fn [{:syms [?ref ?type]}]
+          ;; TODO: bug in attr-bindings - now need to check for attr existence
+          (when ?ref
             [:a {:href  ?ref
-                 :title (m/app ref-title ?type)}
-             [:slot]])
-          (constantly)))
+                 :title (da-type ?type)}
+             [:slot]]))))
 
-(def postprocess
-  (memoize #(hic/postprocess % {:prefix prefix, :injector injector})))
+(def wrap-pbs
+  (cup/transformer
+    :from '[:div * [:<> [:pb] +]]
+    :to (fn [{:syms [<>] :as bindings}]
+          (let [{:keys [begin end]} (meta <>)
+                source (:source (meta bindings))]
+            (vec (concat (subvec source 0 begin)
+                         [(into [:pbs] (subvec source begin end))]
+                         (subvec source end)))))))
+
+(def stages
+  [{:transformers [wrap-pbs]}
+   {:transformers [ref-as-anchor
+                   list-as-ul]
+    :wrapper      rescope/shadow-wrapper
+    :default      (helpers/default-fn {:prefix    prefix
+                                       :attr-kmap {:xml:lang :lang
+                                                   :xml:id   :id}})}])
+
+(def rewrite
+  (memoize #(cup/rewrite % stages)))
 
 (def parse
   (memoize xml/parse))
@@ -53,4 +69,4 @@
 (defn tei-xml
   "Parse, postprocess, and display TEI."
   [xml]
-  [rescope/scope (-> xml parse postprocess) css])
+  [rescope/scope (-> xml parse rewrite) css])
