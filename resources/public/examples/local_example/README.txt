1e1e485496f36f82bf571aaac170ecdea592b285
Simon Gray <simongray@gmail.com>
2021-04-06 11:32:37 +0200

    added timeline JS code + embedded timeline in index.html

---
+++ resources/public/examples/local_example/README.txt
@@ -0,0 +1,60 @@
+
+                          Local Timeline
+                          --------------
+                        
+                        
+This example show how to load a timeline from a local data file.
+
+This trick enables us to read the local data file without needing to run
+any sort of web server.
+
+Installation
+------------
+
+Install this directory of files anywhere on your computer.
+You do need internet access.
+No local web server is needed, Timeline library is loaded directly from
+the web.
+
+Running
+-------
+
+Open your web browser, then open the local_example.html file.
+
+  Instructions for specific browsers:
+  
+  * Internet Explorer
+    a) File / Open...   menu option
+    b) Press [Browse] and navigate to the local_example.html file
+    
+  * Firefox, Safari
+    a)File / Open File...  menu option
+    b) Then navigate to the local_example.html file
+    
+  * Chrome: Doesn't seem to offer a file open option. 
+    You can enter a file url specification of 
+    file:///C:/something / something /local_example/local_example.html
+    but that's not so convenient.
+
+You should then see the Timeline displayed in your browser.
+You will need access to the internet to enable your browser to load the 
+Timeline library from the server.
+
+
+Problems and Questions
+----------------------
+
+Please use the mailing list for help:
+
+  http://groups.google.com/group/simile-widgets/
+
+
+LICENSE
+-------
+
+This software is open source with a BSD license. See
+
+  http://www.simile-widgets.org/ 
+  
+for more information.
+
---
+++ resources/public/examples/test_example2/cubism2.js
@@ -0,0 +1,252 @@
+// This data set uses attribute synonyms. The following attribute names are interchangable.
+// The shorter names enable smaller load files.
+//    eid -- eventID
+//      s -- start
+//      e -- end
+//     ls -- latestStart
+//     ee -- earliestEnd
+//      d -- description
+//     de -- durationEvent
+//      t -- title,
+//      c -- classname
+
+{
+'dateTimeFormat': 'iso8601',
+
+'events' : [
+        {'s': '1924',
+        't': 'Barfusserkirche',
+        'd': 'by Lyonel Feininger, American/German Painter, 1871-1956',
+        'image': 'http://images.allposters.com/images/AWI/NR096_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Barfusserkirche-1924-Posters_i1116895_.htm'
+        },
+
+
+        {'s': '1913',
+        'e': '1929',
+        't': 'Three Figures',
+        'd': 'by Kasimir Malevich, Ukrainian Painter, 1878-1935',
+        'image': 'http://images.allposters.com/images/BRGPOD/75857_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Three-Figures-1913-28-Posters_i1349989_.htm'
+        },
+
+
+        {'s': '1881',
+        'e': '1953',
+        't': 'Landschaft bei Montreuil',
+        'd': 'by Albert Gleizes, French Painter, 1881-1953',
+        'image': 'http://images.allposters.com/images/mer/1336_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Landschaft-bei-Montreuil-Posters_i339007_.htm',
+        'isDuration' : true,
+        'icon' : "dark-red-circle.png",        
+        'color' : 'red',
+        'textColor' : 'green'},
+
+
+        {'s': '1920',
+        't': 'Femme au Miroir',
+        'd': 'by Fernand Leger, French Painter, 1881-1955',
+        'image': 'http://images.allposters.com/images/AWI/GMR117_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Femme-au-Miroir-1920-Posters_i141266_.htm'
+        },
+
+
+        {'s': '1903',
+        't': 'The Old Guitarist',
+        'd': 'by Pablo Picasso, Spanish Painter/Sculptor, 1881-1973',
+        'image': 'http://images.allposters.com/images/ESC/AP599_b.jpg',
+        'link': 'http://www.allposters.com/-sp/The-Old-Guitarist-c-1903-Posters_i328746_.htm'
+        },
+
+
+        {'s': '1882',
+        'e': '1964',
+        't': 'Jour',
+        'd': 'by Georges Braque, French Painter, 1882-1963',
+        'image': 'http://images.allposters.com/images/SHD/S1041_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Jour-Posters_i126663_.htm',
+        'color': 'green'
+        },
+
+
+        {'s': '1916',
+        't': 'Still Life with a White Dish',
+        'd': 'by Gino Severini, Italian Painter, 1883-1966',
+        'image': 'http://images.allposters.com/images/MCG/FS1254_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Still-Life-with-a-White-Dish-1916-Posters_i366823_.htm'
+        },
+
+
+        {'s': '1885',
+        'e': '1941',
+        't': 'Rhythm, Joie de Vivre',
+        'd': 'by Robert Delaunay, French Painter, 1885-1941',
+        'image': 'http://imagecache2.allposters.com/images/pic/adc/10053983a_b~Rhythm-Joie-de-Vivre-Posters.jpg',
+        'link': 'http://www.allposters.com/-sp/Rhythm-Joie-de-Vivre-Posters_i1250641_.htm'
+        },
+
+
+        {'s': '1885',
+        'e': '1925',
+        't': 'Castor Et Pollux (no stripe)',
+        'd': 'by Roger de la Fresnaye, French Painter, 1885-1925',
+        'image': 'http://images.allposters.com/images/CORPOD/IX001463_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Castor-Et-Pollux-Posters_i831718_.htm',
+        'caption': "This is the event's caption attribute.",
+        'classname': 'hot_event' 
+        },
+
+
+        {'s': '1912',
+        't': 'Portrait of Pablo Picasso',
+        'd': 'by Juan Gris, Spanish Painter/Sculptor, 1887-1927',
+        'image': 'http://images.allposters.com/images/BRGPOD/156514_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Portrait-of-Pablo-Picasso-1881-1973-1912-Posters_i1344154_.htm'
+        },
+
+
+        {'s': '1891',
+        'e': '1915',
+        't': 'Portrait of Horace Brodsky',
+        'd': 'by Henri Gaudier-Brzeska, French Sculptor, 1891-1915',
+        'image': 'http://imagecache2.allposters.com/images/BRGPOD/102770_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Portrait-of-Horace-Brodsky-Posters_i1584413_.htm'
+        },
+
+        {'s': '4400',
+        't': 'Barfusserkirche',
+        'd': 'by Lyonel Feininger, American/German Painter, 1871-1956',
+        'image': 'http://images.allposters.com/images/AWI/NR096_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Barfusserkirche-1924-Posters_i1116895_.htm'
+        },
+
+
+        {'s': '4400',
+        'e': '4429',
+        't': 'Three Figures',
+        'd': 'by Kasimir Malevich, Ukrainian Painter, 1878-1935',
+        'image': 'http://images.allposters.com/images/BRGPOD/75857_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Three-Figures-1913-28-Posters_i1349989_.htm'
+        },
+
+
+        {'s': '4400',
+        'e': '4453',
+        't': 'Landschaft bei Montreuil',
+        'd': 'by Albert Gleizes, French Painter, 1881-1953',
+        'image': 'http://images.allposters.com/images/mer/1336_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Landschaft-bei-Montreuil-Posters_i339007_.htm',
+        'isDuration' : true,
+        'icon' : "dark-red-circle.png",        
+        'color' : 'red',
+        'textColor' : 'green'},
+
+
+        {'s': '4400',
+        't': 'Femme au Miroir',
+        'd': 'by Fernand Leger, French Painter, 1881-1955',
+        'image': 'http://images.allposters.com/images/AWI/GMR117_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Femme-au-Miroir-1920-Posters_i141266_.htm'
+        },
+
+
+        {'s': '4400',
+        't': 'The Old Guitarist',
+        'd': 'by Pablo Picasso, Spanish Painter/Sculptor, 1881-1973',
+        'image': 'http://images.allposters.com/images/ESC/AP599_b.jpg',
+        'link': 'http://www.allposters.com/-sp/The-Old-Guitarist-c-1903-Posters_i328746_.htm'
+        },
+
+
+        {'s': '4400',
+        'e': '4464',
+        't': 'Jour',
+        'd': 'by Georges Braque, French Painter, 1882-1963',
+        'image': 'http://images.allposters.com/images/SHD/S1041_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Jour-Posters_i126663_.htm',
+        'color': 'green'
+        },
+
+
+        {'s': '4400',
+        't': 'Still Life with a White Dish',
+        'd': 'by Gino Severini, Italian Painter, 1883-1966',
+        'image': 'http://images.allposters.com/images/MCG/FS1254_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Still-Life-with-a-White-Dish-1916-Posters_i366823_.htm'
+        },
+
+
+        {'s': '4400',
+        'e': '4441',
+        't': 'Rhythm, Joie de Vivre',
+        'd': 'by Robert Delaunay, French Painter, 1885-1941',
+        'image': 'http://imagecache2.allposters.com/images/pic/adc/10053983a_b~Rhythm-Joie-de-Vivre-Posters.jpg',
+        'link': 'http://www.allposters.com/-sp/Rhythm-Joie-de-Vivre-Posters_i1250641_.htm'
+        },
+
+
+        {'s': '4400',
+        'e': '4425',
+        't': 'Castor Et Pollux (no stripe)',
+        'd': 'by Roger de la Fresnaye, French Painter, 1885-1925',
+        'image': 'http://images.allposters.com/images/CORPOD/IX001463_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Castor-Et-Pollux-Posters_i831718_.htm',
+        'caption': "This is the event's caption attribute.",
+        'classname': 'hot_event' 
+        },
+
+
+        {'s': '4400',
+        't': 'Portrait of Pablo Picasso',
+        'd': 'by Juan Gris, Spanish Painter/Sculptor, 1887-1927',
+        'image': 'http://images.allposters.com/images/BRGPOD/156514_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Portrait-of-Pablo-Picasso-1881-1973-1912-Posters_i1344154_.htm'
+        },
+
+
+        {'s': '4400',
+        'e': '4415',
+        't': 'Portrait of Horace Brodsky',
+        'd': 'by Henri Gaudier-Brzeska, French Sculptor, 1891-1915',
+        'image': 'http://imagecache2.allposters.com/images/BRGPOD/102770_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Portrait-of-Horace-Brodsky-Posters_i1584413_.htm'
+        },
+        
+        {'s': '4400',
+        'e': '4441',
+        't': 'Rhythm, Joie de Vivre',
+        'd': 'by Robert Delaunay, French Painter, 1885-1941',
+        'image': 'http://imagecache2.allposters.com/images/pic/adc/10053983a_b~Rhythm-Joie-de-Vivre-Posters.jpg',
+        'link': 'http://www.allposters.com/-sp/Rhythm-Joie-de-Vivre-Posters_i1250641_.htm'
+        },
+
+
+        {'s': '4400',
+        'e': '4425',
+        't': 'Castor Et Pollux (no stripe)',
+        'd': 'by Roger de la Fresnaye, French Painter, 1885-1925',
+        'image': 'http://images.allposters.com/images/CORPOD/IX001463_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Castor-Et-Pollux-Posters_i831718_.htm',
+        'caption': "This is the event's caption attribute.",
+        'classname': 'hot_event' 
+        },
+
+
+        {'s': '4400',
+        't': 'Portrait of Pablo Picasso',
+        'd': 'by Juan Gris, Spanish Painter/Sculptor, 1887-1927',
+        'image': 'http://images.allposters.com/images/BRGPOD/156514_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Portrait-of-Pablo-Picasso-1881-1973-1912-Posters_i1344154_.htm'
+        },
+
+
+        {'s': '4400',
+        'e': '4415',
+        't': 'Portrait of Horace Brodsky',
+        'd': 'by Henri Gaudier-Brzeska, French Sculptor, 1891-1915',
+        'image': 'http://imagecache2.allposters.com/images/BRGPOD/102770_b.jpg',
+        'link': 'http://www.allposters.com/-sp/Portrait-of-Horace-Brodsky-Posters_i1584413_.htm'
+        }
+        
+]
+}
